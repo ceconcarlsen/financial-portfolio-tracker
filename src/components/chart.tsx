3c62c4b06b7622385d3d1b4012b9fec21b443e5f
Cecon <cecon.carlsen@unesp.br>
2025-04-01 14:44:16 -0300

    refactor: chart font size

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -30,9 +30,7 @@ const Chart: React.FC<ChartProps> = ({ title }) => {
           <CardTitle>{title}</CardTitle>
         </CardHeader>
         <CardContent className="pl-2">
-          <p className="ml-4 text-sm text-muted-foreground">
-            Failed to load chart data.
-          </p>
+          <p className="ml-4 text-sm text-muted-foreground">Failed to load chart data.</p>
         </CardContent>
       </Card>
     );
@@ -47,32 +45,19 @@ const Chart: React.FC<ChartProps> = ({ title }) => {
         {chartData.length > 0 ? (
           <ResponsiveContainer width="100%" height={350}>
             <BarChart data={chartData}>
-              <XAxis
-                dataKey="portfolioName"
-                stroke="#888888"
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}
-              />
+              <XAxis dataKey="portfolioName" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
               <YAxis
                 stroke="#888888"
-                fontSize={12}
+                fontSize={10}
                 tickLine={false}
                 axisLine={false}
                 tickFormatter={(value) => `${value.toFixed(2)} PLN`}
               />
-              <Bar
-                dataKey="totalPLN"
-                fill="currentColor"
-                radius={[4, 4, 0, 0]}
-                className="fill-primary"
-              />
+              <Bar dataKey="totalPLN" fill="currentColor" radius={[4, 4, 0, 0]} className="fill-primary" />
             </BarChart>
           </ResponsiveContainer>
         ) : (
-          <p className="ml-4 text-sm text-muted-foreground">
-            No data available for portfolio performance.
-          </p>
+          <p className="ml-4 text-sm text-muted-foreground">No data available for portfolio performance.</p>
         )}
       </CardContent>
     </Card>
